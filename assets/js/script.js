5b0102ead15d4f7fd77523c86c05466f59151df1
mattkohl82 <mattkohl82@gmail.com>
2020-04-18 15:31:52 -0500

    its saving to local but cant get it to come back out

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,148 @@ function infoColorChange() {
 
 infoColorChange();
 let hour = setInterval(infoColorChange, 10000)
+
+
+
+
+var tasks = [];
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+        if (!tasks) {
+            tasks = {
+                toDo: []
+            };
+        }
+};
+
+
+
+// save button in modal was clicked
+$(".saveBtn").click(function() {
+    // get form values
+    var taskText = $(".info").val();
+    
+    // save in tasks array
+      tasks.toDo.push({
+        text: taskText,
+      });
+  
+      saveTasks();
+    
+});
+
+loadTasks();
+
+
+
+
+
+
+
+$("#save-info-1").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-2").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-3").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-4").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-5").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-6").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-7").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-8").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-9").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-10").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-11").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-12").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-13").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-14").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-15").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-16").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-17").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-18").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-19").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-20").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-21").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-22").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-23").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+$("#save-info-24").click(function() {
+    alert( "Task has been saved!" );
+  });
+
+
+
+
+
+
