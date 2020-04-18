f5912ab1ea75b0e4bb8fe360daeec0f104aa0826
mattkohl82 <mattkohl82@gmail.com>
2020-04-18 16:50:59 -0500

    storage works i think

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,43 +35,38 @@ let hour = setInterval(infoColorChange, 10000)
 
 
 
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+    var hour = $(this).parent().attr("id");
+    var info = $(this).siblings(".info").val();
+    localStorage.setItem(hour,info);
+    });
+})
 
-var tasks = [];
-
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
-var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-
-        if (!tasks) {
-            tasks = {
-                toDo: []
-            };
-        }
-};
-
-
-
-// save button in modal was clicked
-$(".saveBtn").click(function() {
-    // get form values
-    var taskText = $(".info").val();
-    
-    // save in tasks array
-      tasks.toDo.push({
-        text: taskText,
-      });
-  
-      saveTasks();
-    
-});
-
-loadTasks();
-
-
-
+$("#t-1 .info").val(localStorage.getItem("t-1"));
+$("#t-2 .info").val(localStorage.getItem("t-2"));
+$("#t-3 .info").val(localStorage.getItem("t-3"));
+$("#t-4 .info").val(localStorage.getItem("t-4"));
+$("#t-5 .info").val(localStorage.getItem("t-5"));
+$("#t-6 .info").val(localStorage.getItem("t-6"));
+$("#t-7 .info").val(localStorage.getItem("t-7"));
+$("#t-8 .info").val(localStorage.getItem("t-8"));
+$("#t-9 .info").val(localStorage.getItem("t-9"));
+$("#t-10 .info").val(localStorage.getItem("t-10"));
+$("#t-11 .info").val(localStorage.getItem("t-11"));
+$("#t-12 .info").val(localStorage.getItem("t-12"));
+$("#t-13 .info").val(localStorage.getItem("t-13"));
+$("#t-14 .info").val(localStorage.getItem("t-14"));
+$("#t-15 .info").val(localStorage.getItem("t-15"));
+$("#t-16 .info").val(localStorage.getItem("t-16"));
+$("#t-17 .info").val(localStorage.getItem("t-17"));
+$("#t-18 .info").val(localStorage.getItem("t-18"));
+$("#t-19 .info").val(localStorage.getItem("t-19"));
+$("#t-20 .info").val(localStorage.getItem("t-20"));
+$("#t-21 .info").val(localStorage.getItem("t-21"));
+$("#t-22 .info").val(localStorage.getItem("t-22"));
+$("#t-23 .info").val(localStorage.getItem("t-23"));
+$("#t-24 .info").val(localStorage.getItem("t-24"));
 
 
 
@@ -171,9 +166,3 @@ $("#save-info-23").click(function() {
 $("#save-info-24").click(function() {
     alert( "Task has been saved!" );
   });
-
-
-
-
-
-
